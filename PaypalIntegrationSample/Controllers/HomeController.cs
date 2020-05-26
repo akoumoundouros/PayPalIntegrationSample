3609fad386f69ec21bea692833f903f9b9284f16
akoumoundouros <alex.koumoundouros@programmed.com.au>
2020-05-27 00:39:54 +1000

    Initial Commit

---
+++ PaypalIntegrationSample/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace PaypalIntegrationSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.PayPalClientId = _config.GetValue<string>("ApplicationSettings:PayPalClientId");
+            return View();
+        }
+
+        public IActionResult Privacy() => View();
+
+        public IActionResult Error() => View();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PayPalIntegrationSample
 Sample pay with PayPal integration
+
+Simple .NET Core MVC Application demonstrating the pay with PayPal feature.
+
+The API keys in appsettings.json are linked to a PayPal Sandbox.
